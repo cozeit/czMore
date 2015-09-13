714ac7c39fdddc4fc1ade90251f603617e28dcdf
yatabani <yasiratabani@gmail.com>
2015-09-13 14:08:01 +0300

    Version 1.5.3.2
    Support for removing items, and new features, and events

---
+++ js/jquery.czMore-1.5.3.2.js
@@ -0,0 +1,141 @@
+//Title: Cozeit More plugin by Yasir Atabani
+//Documentation: na
+//Author: Yasir O. Atabani
+//Website: http://www.cozeit.com
+//Twitter: @yatabani
+//
+//Version 1.0.0 Apr 29th 2013 -First Release.
+//Version 1.5.0 Jun 24th 2013 -Second Release.
+//Version 1.5.1 Jul  8th 2013 -Third Release.
+//Version 1.5.2 Jan  7th 2015 -Fourth Release.
+//Version 1.5.3 Mar  8th 2015 -Fifth Release.
+(function ($, undefined) {
+    $.fn.czMore = function (options) {
+
+        //Set defauls for the control
+        var defaults = {
+            max: 5,
+            min: 0,
+            onLoad: null,
+            onAdd: null,
+            onDelete: null
+        };
+        //Update unset options with defaults if needed
+        var options = $.extend(defaults, options);
+        $(this).bind("onAdd", function (event, data) {
+            options.onAdd.call(event, data);
+        });
+        $(this).bind("onLoad", function (event, data) {
+            options.onLoad.call(event, data);
+        });
+        $(this).bind("onDelete", function (event, data) {
+            options.onDelete.call(event, data);
+        });
+        //Executing functionality on all selected elements
+        return this.each(function () {
+            var obj = $(this);
+            var i = obj.children(".recordset").size();
+            var divPlus = '<div id="btnPlus" />';
+            var count = '<input id="' + this.id + '_czMore_txtCount" name="' + this.id + '_czMore_txtCount" type="hidden" value="0" size="5" />';
+
+            obj.before(count);
+            var recordset = obj.children("#first");
+            obj.after(divPlus);
+            var set = recordset.children(".recordset").children().first();
+            var btnPlus = obj.siblings("#btnPlus");
+
+            btnPlus.css({
+                'float': 'right',
+                'border': '0px',
+                'background-image': 'url("img/add.png")',
+                'background-position': 'center center',
+                'background-repeat': 'no-repeat',
+                'height': '25px',
+                'width': '25px',
+                'cursor': 'pointer'
+            });
+
+            if (recordset.length) {
+                obj.siblings("#btnPlus").click(function () {
+                    var i = obj.children(".recordset").size();
+                    var item = recordset.clone().html();
+                    i++;
+                    item = item.replace(/\[([0-9]\d{0})\]/g, "[" + i + "]");
+                    item = item.replace(/\_([0-9]\d{0})\_/g, "_" + i + "_");
+                    //$(element).html(item);
+                    //item = $(item).children().first();
+                    //item = $(item).parent();
+
+                    obj.append(item);
+                    loadMinus(obj.children().last());
+                    minusClick(obj.children().last());
+                    if (options.onAdd != null) {
+                        obj.trigger("onAdd", i);
+                    }
+
+                    obj.siblings("input[name$='czMore_txtCount']").val(i);
+                    return false;
+                });
+                recordset.remove();
+                for (var j = 0; j <= i; j++) {
+                    loadMinus(obj.children()[j]);
+                    minusClick(obj.children()[j]);
+                    if (options.onAdd != null) {
+                        obj.trigger("onAdd", j);
+                    }
+                }
+
+                if (options.onLoad != null) {
+                    obj.trigger("onLoad", i);
+                }
+                //obj.bind("onAdd", function (event, data) {
+                //If you had passed anything in your trigger function, you can grab it using the second parameter in the callback function.
+                //});
+            }
+
+            function resetNumbering() {
+                $(obj).children(".recordset").each(function (index, element) {
+                   $(element).find('input:text, input:password, input:file, select, textarea').each(function(){
+                        old_name = this.name;
+                        new_name = old_name.replace(/\_([0-9]\d{0})\_/g, "_" + (index + 1) + "_");
+                        this.id = this.name = new_name;
+                        //alert(this.name);
+                    });
+                    index++
+                    minusClick(element);
+                });
+            }
+
+            function loadMinus(recordset) {
+                var divMinus = '<div id="btnMinus" />';
+                $(recordset).children().first().before(divMinus);
+                var btnMinus = $(recordset).children("#btnMinus");
+                btnMinus.css({
+                    'float': 'right',
+                    'border': '0px',
+                    'background-image': 'url("img/remove.png")',
+                    'background-position': 'center center',
+                    'background-repeat': 'no-repeat',
+                    'height': '25px',
+                    'width': '25px',
+                    'cursor': 'poitnter'
+                });
+            }
+
+            function minusClick(recordset) {
+                $(recordset).children("#btnMinus").click(function () {
+                    var i = obj.children(".recordset").size();
+                    var id = $(recordset).attr("data-id")
+                    $(recordset).remove();
+                    resetNumbering();
+                    obj.siblings("input[name$='czMore_txtCount']").val(obj.children(".recordset").size());
+                    i--;
+                    if (options.onDelete != null) {
+                        if (id != null)
+                            obj.trigger("onDelete", id);
+                    }
+                });
+            }
+        });
+    };
+})(jQuery);
