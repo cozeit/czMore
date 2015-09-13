3b2b47e0a59112290d4c9efc6566ba25ecc2fc6a
yatabani <yasiratabani@gmail.com>
2015-09-13 16:50:09 +0300

    License
    License

--- js/jquery.czMore-1.5.3.2.js
+++ js/jquery.czMore-1.5.3.2.js
@@ -1,14 +1,37 @@
-//Title: Cozeit More plugin by Yasir Atabani
-//Documentation: na
-//Author: Yasir O. Atabani
-//Website: http://www.cozeit.com
-//Twitter: @yatabani
-//
-//Version 1.0.0 Apr 29th 2013 -First Release.
-//Version 1.5.0 Jun 24th 2013 -Second Release.
-//Version 1.5.1 Jul  8th 2013 -Third Release.
-//Version 1.5.2 Jan  7th 2015 -Fourth Release.
-//Version 1.5.3 Mar  8th 2015 -Fifth Release.
+/*
+Title: Cozeit More plugin by Yasir Atabani
+Documentation: na
+Author: Yasir O. Atabani
+Website: http://www.cozeit.com
+Twitter: @yatabani
+Copyright (c) 2015 Cozeit, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+REVISIONS
+-----------------------------------------------------------------------------
+Version 1.0.0 Apr 29th 2013 -First Release.
+Version 1.5.0 Jun 24th 2013 -Second Release.
+Version 1.5.1 Jul  8th 2013 -Third Release.
+Version 1.5.2 Jan  7th 2015 -Fourth Release.
+Version 1.5.3 Mar  8th 2015 -Fifth Release.
+*/
 (function ($, undefined) {
     $.fn.czMore = function (options) {
 
